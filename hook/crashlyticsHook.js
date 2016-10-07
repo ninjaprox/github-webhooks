def555553146019b1589fe8fca7efe1b41cbcddb
Vinh Nguyen <ninjaprox@gmail.com>
2016-10-07 10:26:41 +0000

    New crashlyticsClient

--- hook/crashlyticsHook.js
+++ hook/crashlyticsHook.js
@@ -1,8 +1,5 @@
-require("dotenv").config();
-
 const express = require("express");
 const router = express.Router();
-const phantom = require("phantom");
 
 module.exports = router;
 
@@ -14,65 +11,6 @@ router.post("/", function(req, res) {
         return;
     }
 
-    const issueUrl = req.body.payload.url;
-    const email = process.env.FABRIC_EMAIL;
-    const password = process.env.FABRIC_PASSWORD;
-    var page;
-    var phantomInstance;
-
-    phantom.create(["--load-images=no"])
-        .then(function(instance) {
-            phantomInstance = instance;
-
-            return instance.createPage();
-        })
-        .then(function(_page) {
-            page = _page;
-
-            return page.open(issueUrl);
-        })
-        .then(function(status) {
-            return page.on("onUrlChanged", loginHandler(phantomInstance, page, function(error, content) {
-                // console.log(content);
-                // Use content here
-            }));
-        })
-        .then(function() {
-            return page.evaluate(function(email, password) {
-                $("#email").val(email);
-                $("#password").val(password);
-                $("[type=submit]").submit();
-            }, email, password);
-        })
-        .catch(function(error) {
-            console.log(error);
-            phantomInstance.exit();
-        });
-
     console.log(req.body);
     res.status(200).end();
 });
-
-function loginHandler(phantom, page, callback) {
-    return function(url) {
-        setTimeout(function() {
-            page.property("content")
-                .then(function(content) {
-                    if (callback) {
-                        callback(null, content);
-                    }
-
-                    page.close();
-                    phantom.exit();
-                })
-                .catch(function(error) {
-                    if (callback) {
-                        callback(error);
-                    }
-
-                    console.log(error);
-                    phantom.exit();
-                });
-        }, 5000);
-    };
-}
