51e484022c916c8b97e48f539a1750ff5b30a959
Vinh Nguyen <ninjaprox@gmail.com>
2016-10-07 08:25:49 +0000

    Log in Crashlytics and get content

--- hook/crashlyticsHook.js
+++ hook/crashlyticsHook.js
@@ -1,47 +1,78 @@
+require("dotenv").config();
+
 const express = require("express");
 const router = express.Router();
-const request = require("request");
 const phantom = require("phantom");
 
 module.exports = router;
 
+/*global $*/
 router.post("/", function(req, res) {
+    if (req.body.event === "verification") {
+        res.status(200).end();
+
+        return;
+    }
+
     const issueUrl = req.body.payload.url;
+    const email = process.env.FABRIC_EMAIL;
+    const password = process.env.FABRIC_PASSWORD;
+    var page;
+    var phantomInstance;
 
-    // request(issueUrl, function(error, response, body) {
-    //     const versions = [] || body.match(/(\d+\.\d+\.\d+ \(\d+\))/gm);
+    phantom.create(["--load-images=no"])
+        .then(function(instance) {
+            phantomInstance = instance;
 
-    //     console.log(body);
-    // });
+            return instance.createPage();
+        })
+        .then(function(_page) {
+            page = _page;
 
-    var sitepage = null;
-    var phInstance = null;
-    
-    // phantom.create(["--load-images=no"], {
-    //         phantomPath: "/home/ubuntu/downloads/phantomjs-2.1.1-linux-x86_64/bin/phantomjs"
-    //     })
-    //     .then(instance => {
-    //         phInstance = instance;
-    //         return instance.createPage();
-    //     })
-    //     .then(page => {
-    //         sitepage = page;
-    //         return page.open(issueUrl);
-    //     })
-    //     .then(status => {
-    //         console.log(status);
-    //         return sitepage.property('content');
-    //     })
-    //     .then(content => {
-    //         console.log(content);
-    //         sitepage.close();
-    //         phInstance.exit();
-    //     })
-    //     .catch(error => {
-    //         console.log(error);
-    //         phInstance.exit();
-    //     });
+            return page.open(issueUrl);
+        })
+        .then(function(status) {
+            return page.on("onUrlChanged", loginHandler(phantomInstance, page, function(error, content) {
+                // console.log(content);
+                // Use content here
+            }));
+        })
+        .then(function() {
+            return page.evaluate(function(email, password) {
+                $("#email").val(email);
+                $("#password").val(password);
+                $("[type=submit]").submit();
+            }, email, password);
+        })
+        .catch(function(error) {
+            console.log(error);
+            phantomInstance.exit();
+        });
 
-    console.log(res.body);
+    console.log(req.body);
     res.status(200).end();
 });
+
+function loginHandler(phantom, page, callback) {
+    return function(url) {
+        setTimeout(function() {
+            page.property("content")
+                .then(function(content) {
+                    if (callback) {
+                        callback(null, content);
+                    }
+
+                    page.close();
+                    phantom.exit();
+                })
+                .catch(function(error) {
+                    if (callback) {
+                        callback(error);
+                    }
+
+                    console.log(error);
+                    phantom.exit();
+                });
+        }, 5000);
+    };
+}
