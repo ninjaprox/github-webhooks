a5492da748443934c5645141e1fd4963d826e5d3
Vinh Nguyen <ninjaprox@gmail.com>
2016-10-10 01:57:59 +0800

    Configure logger for test

--- logger/logger.js
+++ logger/logger.js
@@ -1,15 +1,25 @@
 const bunyan = require("bunyan");
-const log = bunyan.createLogger({
-    name: process.env.npm_package_name,
-    level: process.env.LOG_LEVEL,
-    streams: [{
-        stream: process.stdout,
-        level: process.env.LOG_LEVEL
-    }, {
-        path: "./.log/server.log",
-        level: process.env.LOG_LEVEL
-    }],
-    serializers: bunyan.stdSerializers
-});
+const isTest = !process.env.npm_package_name;
+var log;
+
+if (!isTest) {
+    log = bunyan.createLogger({
+        name: process.env.npm_package_name,
+        level: process.env.LOG_LEVEL,
+        streams: [{
+            stream: process.stdout,
+            level: process.env.LOG_LEVEL
+        }, {
+            path: "./.log/server.log",
+            level: process.env.LOG_LEVEL
+        }],
+        serializers: bunyan.stdSerializers
+    });
+} else {
+    log = bunyan.createLogger({
+        name: "TEST",
+        level: "debug"
+    });
+}
 
 module.exports = log;
