2bde7c7c064b510e9a4c3c4137e02a74cb04dce7
Vinh Nguyen <ninjaprox@gmail.com>
2016-10-10 00:03:22 +0800

    Log requests to /hook

--- hook/hook.js
+++ hook/hook.js
@@ -2,8 +2,16 @@ const express = require("express");
 const router = express.Router();
 const githubHook = require("./githubHook");
 const crashlyticsHook = require("./crashlyticsHook");
+const log = require("../logger/logger");
 
 module.exports = router;
 
+router.use("/", function(req, res, next) {
+    log.info({
+        req: req,
+        body: req.body
+    });
+    next();
+});
 router.use("/github", githubHook);
 router.use("/crashlytics", crashlyticsHook);
