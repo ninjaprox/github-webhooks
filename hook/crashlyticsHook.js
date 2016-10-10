858de734811e39e2c2f921f7739a726debc44d7f
Vinh Nguyen <ninjaprox@gmail.com>
2016-10-11 00:20:52 +0800

    Create GitHub issue on Crashlytics issue

--- hook/crashlyticsHook.js
+++ hook/crashlyticsHook.js
@@ -1,10 +1,15 @@
+require("dotenv").config();
+
 const express = require("express");
 const router = express.Router();
 const CrashlyticsClient = require("../lib/crashlyticsClient");
 const Q = require("q");
 const log = require("../logger/logger");
+const GithubClient = require("../lib/githubClient");
 
 module.exports = router;
+
+const githubClient = new GithubClient(process.env.GITHUB_TOKEN, process.env.GITHUB_REPO);
 
 /*global $*/
 router.post("/", function(req, res) {
@@ -30,7 +35,7 @@ router.post("/", function(req, res) {
 
             issue.title = `Crash in ${titleComponents[0]}`;
             issue.file = titleComponents[0];
-            issue.line = titleComponents[2] || "unknown";
+            issue.line = titleComponents[1] || "unknown";
             issue.versions = versions;
             issue.exception = exception;
 
@@ -38,9 +43,30 @@ router.post("/", function(req, res) {
                 issue: issue
             }, "Crashlytics issue detail");
 
-            return client;
+            return [client, issue];
         })
-        .then(function(client) {
+        .spread(function(client, issue) {
+            const body = ["### File",
+                `${issue.file}`,
+                "### Method",
+                `${issue.method}`,
+                "### Line",
+                `${issue.line}`,
+                "### Exception",
+                `${issue.exception.exception}`,
+                `*${issue.exception.info}*`,
+                "### Source",
+                `[${issue.url}](${issue.url})`
+            ].join("\r\n");
+            var labels = issue.versions;
+
+            labels.push("crashlytics");
+            return [client, githubClient.createIssue(issue.title, {
+                body: body,
+                labels: labels
+            })];
+        })
+        .spread(function(client) {
             client.done();
         })
         .catch(function(error) {
